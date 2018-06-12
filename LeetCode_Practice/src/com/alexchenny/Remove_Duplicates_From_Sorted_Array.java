576e1228ed780df4f4f0dc332c655d1953f2713f
Alex Chen <wqchen@cpp.edu>
2018-06-12 12:59:33 -0500

    Third commit - implemented Remove_Duplicate Method

--- LeetCode_Practice/src/com/alexchenny/Remove_Duplicates_From_Sorted_Array.java
+++ LeetCode_Practice/src/com/alexchenny/Remove_Duplicates_From_Sorted_Array.java
@@ -1,5 +1,29 @@
 package com.alexchenny;
 
 public class Remove_Duplicates_From_Sorted_Array {
+	
+//	TESTER METHOD
+//	
+//	public static void main (String[] args) {
+//		int[] nums = {0,0,1,1,1,2,2,3,3,4};
+//		Remove_Duplicates_From_Sorted_Array testObj = new Remove_Duplicates_From_Sorted_Array();
+//		int length = testObj.removeDuplicates(nums);
+//		System.out.println(length);
+//	}
 
+	  public int removeDuplicates(int[] nums) {
+			if (nums.length == 0) 
+				return 0;
+			int a = 0;
+		    int b = 1;
+		    while (b<nums.length) {
+		    	if (nums[a] != nums[b]) {
+		    		a++;
+		    		nums[a] = nums[b];
+		    	}
+		    	b++;
+		    }
+		    return a+1;
+		}
+	
 }
--- LeetCode_Practice/src/com/alexchenny/Tester.java
+++ LeetCode_Practice/src/com/alexchenny/Tester.java
@@ -1,5 +1,5 @@
 package com.alexchenny;
 
 public class Tester {
-
+	
 }
